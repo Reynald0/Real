3b44456d56b803aa9ee6d074781ae8fbfbfdcf9f
Reynald0 <reynaldobernard15@gmail.com>
2016-01-14 13:02:53 -0600

    Base de datos

--- Real/settings/production.py
+++ Real/settings/production.py
@@ -6,13 +6,10 @@ ALLOWED_HOSTS = ['127.0.0.1' , 'reynald0.pythonanywhere.com']
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'real_db',
-        'USER': 'Reynald0',
-        'PASSWORD': 'R3ynaldit095',
-        'HOST': 'Reynald0.mysql.pythonanywhere-services.com',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_DIR, 'real_db.db'),
     }
 }
--- Real/wsgi.py
+++ Real/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Real.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Real.settings.production")
 
 application = get_wsgi_application()
