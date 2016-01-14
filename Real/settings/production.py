317d9501c04de7691395553bc057e8fb4eab105e
Reynald0 <reynaldobernard15@gmail.com>
2016-01-14 13:17:13 -0600

    Base de datos

--- Real/settings/production.py
+++ Real/settings/production.py
@@ -6,10 +6,9 @@ ALLOWED_HOSTS = ['127.0.0.1' , 'reynald0.pythonanywhere.com']
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_DIR, 'real_db.db'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
