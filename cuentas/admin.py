b711d616ef44e5c4b167ba6adf69ec544ad788ce
Reynald0 <reynaldobernard15@gmail.com>
2016-01-19 03:40:13 -0600

    Administrador de sitio
    * Se muestran los elementos relevantes en el administrador de sitio.

--- cuentas/admin.py
+++ cuentas/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Alumno, Noticia
 
-admin.site.register(Alumno)
+class AlumnoAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'no_control', 'nombre', 'apellido', 'edad', 'carrera', 'promedio', 'semestre')
+
+admin.site.register(Alumno, AlumnoAdmin)
 admin.site.register(Noticia)
