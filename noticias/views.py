882bb75e472e458ecb25f7f7be5dc302aece1015
Reynald0 <reynaldobernard15@gmail.com>
2017-04-21 09:51:58 -0500

    Lista de noticias
    * EL HTML lista de noticias ahora muestra un mensaje si hay o no
    noticias. Anteriormente tiraba error si no existía alguna.
    
    * La vista (views.py) es corregida levemente.
    
    * Eliminaciób de CSS materialize, ya que esta no se linkea en la
    base.html.

--- noticias/views.py
+++ noticias/views.py
@@ -6,6 +6,6 @@ def noticias(request):
     if not request.user.is_authenticated(): #Si el usuario NO esta autenticado (logeado)
         return login_alumno(request) #Entonces lo manda al login de alumnos
     # Obtiene todos los objetos noticias y los filtra por fecha de publicacion desde el mas actual (arriba) hasta el mas antiguo (abajo)
-    noticias = Noticia.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('-fecha_publicacion')
+    noticias = Noticia.objects.all().filter(fecha_publicacion__lte=timezone.now()).order_by('-fecha_publicacion')
     #Regresa el html lista_noticias renderizado y envia la lista de las noticias al html lista_noticias
     return render(request, 'noticias/lista_noticias.html', {'noticias': noticias})
