fd5d06947e73df60b87ffb282a96d1204190a792
Reynald0 <reynaldobernard15@gmail.com>
2017-04-22 09:52:39 -0500

    Perfil de Administrador
    * El administrador no posee un alumno registrado por lo que se muestra
    un mensaje al visitar el perfil.

--- cuentas/static/js/sitio.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('.button-collapse').sideNav();
-  $('select').material_select();
-  $(".dropdown-button").dropdown({
-    belowOrigin: true, // Displays dropdown below the button
-    hover: true, // Activate on hover
-  });
-});
