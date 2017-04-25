482ea5edd595e5a92107e836d0bdc28851bfb296
Reynald0 <reynaldobernard15@gmail.com>
2017-04-24 21:49:03 -0500

    Encabezado de Sitio de administracion de Django CAMBIADO

--- cuentas/admin.py
+++ cuentas/admin.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 from django.contrib import admin
 from .models import Alumno
 
@@ -5,3 +6,5 @@ class AlumnoAdmin(admin.ModelAdmin):
     list_display = ('user_id', 'no_control', 'nombre', 'apellido', 'fecha_nac', 'carrera', 'promedio', 'semestre')
 
 admin.site.register(Alumno, AlumnoAdmin)
+
+admin.site.site_header = "SISTEMA REAL"
