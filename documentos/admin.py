990e4505b936899cf9dece6497f5da515d07702d
Reynald0 <reynaldobernard15@gmail.com>
2017-04-24 21:50:39 -0500

    Modelos de la aplicacion DOCUMENTOS en ADMINSITE

--- documentos/admin.py
+++ documentos/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import ComprobanteDomicilio, CredencialEstudiante, Kardex
 
-# Register your models here.
+
+class ComprobanteDomicilioAdmin(admin.ModelAdmin):
+    list_display = ('alumno', 'url_documento')
+
+
+class CredencialEstudianteAdmin(admin.ModelAdmin):
+    list_display = ('alumno', 'url_documento')
+
+
+class KardexAdmin(admin.ModelAdmin):
+    list_display = ('alumno', 'url_documento')
+
+
+admin.site.register(ComprobanteDomicilio, ComprobanteDomicilioAdmin)
+admin.site.register(CredencialEstudiante, CredencialEstudianteAdmin)
+admin.site.register(Kardex, KardexAdmin)
