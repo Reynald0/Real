3ac075dafd6b8e50a8c7058334ce2823d7b9e84e
Reynald0 <reynaldobernard15@gmail.com>
2017-05-16 15:20:19 -0500

    Eliminacion de documentos
    ** Cuando se sube otro documento se elimina el anterior

--- documentos/views.py
+++ documentos/views.py
@@ -1,6 +1,8 @@
+import os
 from django.contrib.auth.decorators import login_required
-from django.db import IntegrityError
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404
+
+from Real import settings
 from documentos.forms import ComprobanteDomicilioForm, CredencialEstudianteForm, KardexForm
 from documentos.models import ComprobanteDomicilio, CredencialEstudiante, Kardex
 from cuentas.views import Alumno, perfil_alumno
@@ -9,16 +11,17 @@ from cuentas.views import Alumno, perfil_alumno
 
 @login_required(login_url='login_alumno')
 def comprobante_domicilio(request):
-    usuario_alumno = get_object_or_404(Alumno, user= request.user)
+    usuario_alumno = get_object_or_404(Alumno, user=request.user)
     if request.method == 'POST':  # Si el formulario envia algo con el metodo POST
-        form = ComprobanteDomicilioForm(request.POST, request.FILES)  # Se crea el objeto form, a este objeto se le asigna el modelo LogAlumno
+        form = ComprobanteDomicilioForm(request.POST,
+                          request.FILES)  # Se crea el objeto form, a este objeto se le asigna el modelo LogAlumno
         if form.is_valid():  # Si el formulario es valido
             cleaned_data = form.cleaned_data
-            usuario = request.user
             url_del_documento = cleaned_data.get('url_documento')  # Se obtiene el valor del campo usuario
             # Verifica que se pueda registrar el archivo
             try:
                 archivo = ComprobanteDomicilio.objects.get(alumno=usuario_alumno)
+                os.remove(archivo.url_documento.path)
                 archivo.url_documento = url_del_documento
                 archivo.save()
             except Exception  as e:  # Si el usuario ya existe manda un mensaje
@@ -29,7 +32,7 @@ def comprobante_domicilio(request):
         else:  # De lo contrario, el formulario no es valido
             # Regresa el html 'login' renderizado con la variable form, que es el formulario previamente rellenado
             return render(request, 'documentos/comprobante_domicilio.html', {'form': form})
-    else: # De lo contrario, la persona esta apenas por registrarse
+    else:  # De lo contrario, la persona esta apenas por registrarse
         # Se crea el bojeto form con el formulario LogAlumno
         form = ComprobanteDomicilioForm()
         # Regresa el html 'comprobante_domicilio' renderizado con la variable form en blanco.
@@ -47,6 +50,7 @@ def credencial_estudiante(request):
             # Verifica que se pueda registrar el archivo
             try:
                 archivo = CredencialEstudiante.objects.get(alumno=usuario_alumno)
+                os.remove(archivo.url_documento.path)
                 archivo.url_documento = url_del_documento
                 archivo.save()
             except Exception  as e:  # Si el usuario ya existe manda un mensaje
@@ -76,6 +80,7 @@ def kardex(request):
             # Verifica que se pueda registrar el archivo
             try:
                 archivo = Kardex.objects.get(alumno=usuario_alumno)
+                os.remove(archivo.url_documento.path)
                 archivo.url_documento = url_del_documento
                 archivo.save()
             except Exception  as e:  # Si el usuario ya existe manda un mensaje
