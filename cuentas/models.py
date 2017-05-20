da1c082d92b7672d89eaa07c69d89fd4e584e5dc
Reynald0 <reynaldobernard15@gmail.com>
2017-05-19 22:46:13 -0500

    Regreso de STR
    ** Se regresa STR en los modelos de la app CUENTAS

--- cuentas/models.py
+++ cuentas/models.py
@@ -8,14 +8,14 @@ class Carrera(models.Model):
     id = models.AutoField(primary_key=True) #Atributo id es tipo INT y es auto incrementable
     carrera = models.CharField(max_length=90) #Atributo carrera es tipo de dato Char y su tamanio maximo es 90
 
-    def __unicode__(self):
+    def __str__(self):
         return u"{0}".format(self.carrera)
 
 class Estado_Solicitud(models.Model):
     id = models.AutoField(primary_key=True)
     tipo_solicitud = models.CharField(max_length=30)
 
-    def __unicode__(self):
+    def __str__(self):
         return u"{0}".format(self.tipo_solicitud)
 
 class Alumno(models.Model): #Se crea el objeto Alumno, en la base de datos es una tabla
@@ -31,6 +31,6 @@ class Alumno(models.Model): #Se crea el objeto Alumno, en la base de datos es un
     estado_solicitud = models.OneToOneField(Estado_Solicitud, on_delete=models.CASCADE, default=1) # 1 --> en proceso
     #revisar la tabla estado solicitud
 
-    def __unicode__(self): #funcion __str__ regresa un string para identificar al objeto de los demas
+    def __str__(self): #funcion __str__ regresa un string para identificar al objeto de los demas
         return u"{0} - {1} {2} {3}".format(self.no_control, self.nombre, self.apellido_paterno, self.apellido_materno)
         #Regresa el no_control en forma de string UTF-8
