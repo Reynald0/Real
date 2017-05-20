cb1cfa0db5f07de7f0dc3e20b00833869ea081b1
Reynald0 <reynaldobernard15@gmail.com>
2017-05-20 08:19:12 -0500

    Ver media en produccion

--- Real/settings.py
+++ Real/settings.py
@@ -115,6 +115,7 @@ STATIC_URL = '/static/'
 # Ademas es la que reconocera el servidor al momento de ejecutar collectstatic
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/home/Reynald0/Real/media' # Para produccion
+#MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
